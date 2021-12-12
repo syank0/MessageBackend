8d642aaba5261d5a9a421dc6411a427d7334ca0b
yankovs <shibushka@gmail.com>
2021-12-12 19:11:40 +0200

    Added start of JWT authentication, added User model

--- Messages/models.py
+++ Messages/models.py
@@ -1,10 +1,50 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from Messages import db
+from dataclasses import dataclass
 
 
+@dataclass
+class User(db.Model):
+    __tablename__ = 'users'
+
+    username: str
+    password: str
+
+    username = db.Column(db.String(120), primary_key=True)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = generate_password_hash(password, method='sha256')
+
+    @classmethod
+    def authenticate(cls, **kwargs):
+        username = kwargs.get('username')
+        password = kwargs.get('password')
+
+        if not username or not password:
+            return None
+
+        user = cls.query.filter_by(username=username).first()
+        if not user or not check_password_hash(user.password, password):
+            return None
+
+        return user
+
+
+@dataclass
 class Message(db.Model):
     __tablename__ = 'message'
-    sender = db.Column(db.String(), nullable=False, primary_key=True)
-    # receiver = db.Column(db.String(), nullable=False)
+
+    sender: str
+    receiver: str
+
+    sender = db.Column(db.Text(length=2), nullable=False, primary_key=True)
+    receiver = db.Column(db.Text, nullable=False, primary_key=True)
     # message = db.column(db.String())
     # subject = db.Column(db.String(), nullable=False)
     # date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'sender: {self.sender}'
--- Messages/routes.py
+++ Messages/routes.py
@@ -1,5 +1,49 @@
-from Messages import app
+import jwt
+from datetime import datetime, timedelta
+from Messages import app, db
+from Messages.models import Message, User
+from flask import request, jsonify
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.authenticate(**data)
+
+    if not user:
+        return jsonify({'Error': 'Invalid credentials', 'authenticated': False}), 401
+
+    token = jwt.encode(
+        {
+            'sub': user.username,
+            'iat': datetime.utcnow(),
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        },
+        app.config['SECRET_KEY']
+    )
+
+    return jsonify({'token': token.encode('UTF-8')})
+
+
+@app.route('/message', methods=['POST'])
+def write_message():
+    data = request.json
+    message = Message(sender=data['sender'], receiver=data['receiver'])
+    db.session.add(message)
+    db.session.commit()
+    return jsonify(data)
+
+
+@app.route('/messages', methods=['GET'])
+def get_all_messages():
+    messages = Message.query.all()
+    return jsonify(messages)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    user = User(**data)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user)
